b3dc3050e3a978f5400c5fc4b8a6f0fd22768eb7
flaviostutz <flaviostutz@gmail.com>
2020-02-21 17:57:45 -0300

    managed to restream without reencoding with a simple rtsp server

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,27 @@
+FROM golang:1.13.8-alpine AS BUILD
+
+RUN apk --update add git
+
+WORKDIR /tmp
+RUN git clone https://github.com/aler9/rtsp-simple-server.git
+WORKDIR /tmp/rtsp-simple-server
+
+RUN go mod download
+RUN go build -o /go/bin/rtsp-simple-server .
+
+
+
 FROM jrottenberg/ffmpeg:3.3-alpine
 
-EXPOSE 554
-EXPOSE 8080
-
-RUN apk update && \
-  apk add --no-cache gettext
+EXPOSE 8554
+EXPOSE 8000
+EXPOSE 8001
 
 ENV SOURCE_URL ''
 ENV STREAM_NAME 'stream'
-ENV STREAM_VIDEO_CODEC ''
-ENV STREAM_VIDEO_SIZE ''
-ENV STREAM_VIDEO_FRAMERATE ''
 
-COPY ffserver.conf.template /
+COPY --from=BUILD /go/bin/rtsp-simple-server /bin/rtsp-simple-server
+
 COPY start-relay.sh /
 
 ENTRYPOINT [ "/bin/sh" ]
--- start-relay.sh
+++ start-relay.sh
@@ -7,26 +7,10 @@ if [ "$SOURCE_URL" == "" ]; then
    exit 1
 fi
 
-echo "Preparing ffserver configurations"
-envsubst < /ffserver.conf.template > /etc/ffserver.conf
 
-if [ "$STREAM_CODEC" == "" ]; then
-   sed -i -e "s/VideoCodec/#VideoCodec/g" /etc/ffserver.conf
-fi
-if [ "$STREAM_VIDEO_SIZE" == "" ]; then
-   sed -i -e "s/VideoSize/#VideoSize/g" /etc/ffserver.conf
-fi
-if [ "$STREAM_VIDEO_FRAMERATE" == "" ]; then
-   sed -i -e "s/VideoFrameRate/#VideoFrameRate/g" /etc/ffserver.conf
-fi
+echo "Starting rtsp server daemon..."
+rtsp-simple-server &
+sleep 2
 
-echo "/etc/ffserver.conf"
-more /etc/ffserver.conf
-
-echo "Starting ffserver daemon..."
-echo "Status page at http://:8080/stats.html"
-ffserver &
-
-echo "Start relaying from $SOURCE_URL to RTSP rtsp://[dockerhost]:554/$STREAM_NAME"
-ffmpeg -i $SOURCE_URL -rtsp_transport tcp http://127.0.0.1:8080/feed1.ffm
-
+echo "Start relaying from $SOURCE_URL to RTSP at rtsp://0.0.0.0:8554/$STREAM_NAME"
+ffmpeg -i $SOURCE_URL -c copy -f rtsp rtsp://127.0.0.1:8554/$STREAM_NAME
